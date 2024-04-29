991d6f6fc5a4a676a149d2c31f76824f3095605f
Vitor Ventura <vventura@b-simple.pt>
2024-04-29 14:10:17 +0100

    changed cors to program

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -15,41 +15,40 @@ var connectionString = config.GetConnectionString("AbsanteeDatabase" + args[0]);
 var projectQueueName = config["ProjectQueues:" + args[0]];
 var projectUpdateQueueName = config["ProjectUpdateQueues:" + args[0]];
 
-
-// Add services to the container.
-
 builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
 
 builder.Services.AddDbContext<AbsanteeContext>(opt =>
     opt.UseSqlite(connectionString)
 );
-// builder.Services.AddDbContext<AbsanteeContext>(opt =>
-//     //opt.UseInMemoryDatabase("AbsanteeList")
-//     //opt.UseSqlite("Data Source=AbsanteeDatabase.sqlite")
-//     opt.UseSqlite(Host.CreateApplicationBuilder().Configuration.GetConnectionString("AbsanteeDatabase"))
-// );
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddTransient<IProjectRepository, ProjectRepository>();
 builder.Services.AddTransient<IProjectFactory, ProjectFactory>();
 builder.Services.AddTransient<ProjectMapper>();
-
-builder.Services.AddTransient<ProjectGateway>();
+builder.Services.AddTransient<ProjectService>();
 builder.Services.AddTransient<ProjectGatewayUpdate>();
-// builder.Services.AddTransient<ProjectAMQPService>();
+builder.Services.AddTransient<ProjectGateway>();
 
 builder.Services.AddScoped<ProjectService>();
-
 
 builder.Services.AddSingleton<IRabbitMQConsumerController, RabbitMQConsumerController>();
 builder.Services.AddSingleton<IRabbitMQConsumerUpdateController, RabbitMQConsumerUpdateController>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -58,7 +57,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection(); 
 
+app.UseRouting();
+
+app.UseCors("AllowAllOrigins"); 
+
 app.UseAuthorization();
+
 var rabbitMQConsumerService = app.Services.GetRequiredService<IRabbitMQConsumerController>();
 rabbitMQConsumerService.ConfigQueue(projectQueueName);
 rabbitMQConsumerService.StartConsuming();
